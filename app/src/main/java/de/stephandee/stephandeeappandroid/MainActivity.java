8c2b5e8b56929ce6d6b2d98a8dfc1bfabaaff4fd
Stephan D <duenkel.stephan@gmail.com>
2019-06-18 16:54:25 +0200

    -created the GUI for Products #4

--- app/src/main/java/de/stephandee/stephandeeappandroid/MainActivity.java
+++ app/src/main/java/de/stephandee/stephandeeappandroid/MainActivity.java
@@ -4,25 +4,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import de.stephandee.stephandeeappandroid.models.Product;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<Product> mProduct = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Produkt");
         // toolbar.setSubtitle("StephanDeeApp");
         // toolbar.setLogo(android.R.drawable.);
-
-        RecyclerView productList = findViewById(R.id.productList);
 
         FloatingActionButton fab = findViewById(R.id.addProductButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Product product1 = new Product(
+                "asd",
+                new Date(),
+                "Pepsi",
+                "Erfrischend",
+                2,
+                0);
+        Product product2 = new Product(
+                "dsa",
+                new Date(),
+                "Cola",
+                "Erfrischender",
+                1,
+                0);
+        mProduct.add(product1);
+        mProduct.add(product2);
+
+        initRecyclerView(mProduct);
     }
 
     @Override
@@ -54,5 +82,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initRecyclerView(ArrayList<Product> products) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = findViewById(R.id.productList);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, products);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/de/stephandee/stephandeeappandroid/RecyclerViewAdapter.java
@@ -0,0 +1,74 @@
+package de.stephandee.stephandeeappandroid;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import de.stephandee.stephandeeappandroid.models.Product;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private Context mContext;
+    private ArrayList<Product> mProduct = new ArrayList<>();
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<Product> mProduct) {
+        this.mProduct = mProduct;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        // Log.d(TAG, "onBindViewHolder: called.");
+
+        viewHolder.productName.setText(mProduct.get(i).getName());
+        viewHolder.productDescription.setText(mProduct.get(i).getDescription());
+        viewHolder.productPrice.setText(Float.toString(mProduct.get(i).getPrice()));
+
+        viewHolder.productLayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Log.d(TAG, "onClick: clicked on:" + mProduct.get(i).getName());
+
+                Toast.makeText(mContext, mProduct.get(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProduct.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView productName;
+        TextView productDescription;
+        TextView productPrice;
+        LinearLayout productLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.productName);
+            productDescription = itemView.findViewById(R.id.productDescription);
+            productPrice = itemView.findViewById(R.id.productPrice);
+            productLayout = itemView.findViewById(R.id.productLayout);
+        }
+    }
+}
