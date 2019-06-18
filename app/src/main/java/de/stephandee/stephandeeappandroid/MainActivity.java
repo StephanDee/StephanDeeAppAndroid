eee9fe344af5b61947b23415bbb167112fc25cb3
Stephan D <duenkel.stephan@gmail.com>
2019-06-18 13:47:43 +0200

    - create the gui for Products #4

--- app/src/main/java/de/stephandee/stephandeeappandroid/MainActivity.java
+++ app/src/main/java/de/stephandee/stephandeeappandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -17,8 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Produkt");
+        // toolbar.setSubtitle("StephanDeeApp");
+        // toolbar.setLogo(android.R.drawable.);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        RecyclerView productList = findViewById(R.id.productList);
+
+        FloatingActionButton fab = findViewById(R.id.addProductButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
