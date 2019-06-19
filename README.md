961a695a3793cf5f0dc2d392e25177dd01d760b4
Stephan D <duenkel.stephan@gmail.com>
2019-06-19 14:31:28 +0200

    - added some documentation about retrieving data via StephanDeeCloud REST API. #5

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StephanDeeAppAndroid
 My Frontend - Android Application
+
+# Retrieving Data
+In order to retrieve data from the StephanDeeCloud REST API you have to configure the APIUtils file. [app/src/main/de.stephandee.stephandeeappandroid/dbaccess/APIUtils]
+Replace the API_URL with your IP Adress where the StephanDeeCloud is running.
+You can get your ip address through the commandline tool with the command prompt ipconfig. [IPv4 Adress]
--- app/src/main/java/de/stephandee/stephandeeappandroid/dbaccess/APIUtils.java
+++ app/src/main/java/de/stephandee/stephandeeappandroid/dbaccess/APIUtils.java
@@ -2,11 +2,11 @@ package de.stephandee.stephandeeappandroid.dbaccess;
 
 public class APIUtils {
 
-    // replace localhost with your IP Address - command prompt - ipconfig - IPv4 Adresse
+    // replace localhost with your IP Address - command prompt - ipconfig - IPv4 Adress
     public static final String API_URL = "http://localhost:3000"; // http://localhost:3000
 
     private APIUtils(){
-    };
+    }
 
     public static IProductService getProductService() {
         return RetrofitClient.getClient(API_URL).create(IProductService.class);
